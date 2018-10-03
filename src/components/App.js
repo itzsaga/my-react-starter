61d3aa54cf982a7a0c49283414ec5212fd2c6182
Seth <sethalexander@pm.me>
2018-10-02 21:29:55 -0500

    Add in some state

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react'
-import { hot } from 'react-hot-loader'
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 
-import { jsComponent as HelloFromReason } from './HelloFromReason.bs.js';
+import { jsComponent as HelloFromReason } from "./HelloFromReason.bs.js";
 
 class App extends Component {
+  state = { value: "" };
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <h1>Welcome to My Starter App</h1>
         <h3>It has hot module reloading built in!</h3>
-        <HelloFromReason name="Seth" />
+        <label htmlFor="name">
+          Name:{' '}
+          <input
+            id="name"
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        {<HelloFromReason name={this.state.value} />}
       </div>
-    )
+    );
   }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
